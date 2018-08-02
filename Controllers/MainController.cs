8dca9dd850700e3f2dba0346b2df8a97865f490c
unknown <steelst0@gmail.com>
2018-08-02 13:49:04 -0700

    functional

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,20 +1,49 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 namespace DojoSurvey.Controllers
 {
     public class MainController : Controller
     {
         [HttpGet]
-        [Route("/")]
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        [Route("/result")]
-        public IActionResult Result(string Name, string Location, string FavLang, string Comment="")
+        [Route("result")]
+        public IActionResult Result(string username, string location, string favlang, string comment)
         {
+            string defName = "John Doe";
+            if(username == null)
+            {
+                ViewBag.Name=defName;
+                ViewBag.Location=location;
+                ViewBag.FavLang=favlang;
+                ViewBag.Comment=comment;
+            }
+            else
+            {
+                ViewBag.Name=username;
+                ViewBag.Location=location;
+                ViewBag.FavLang=favlang;
+                ViewBag.Comment=comment;
+            }
+    
+            return View();
+        }
+
+        public IActionResult Result(string location, string favlang)
+        {
+            string username="John";
+            string comment="2Cool4U";
+            ViewBag.Name=username;
+            ViewBag.Location=location;
+            ViewBag.FavLang=favlang;
+            ViewBag.Comment=comment;
+            Console.WriteLine("WE IN HERRRRRRRRRRRRRRRRRREEEEEEEEEEEEEEEEEEEEEEE");
             return View();
         }
     }
