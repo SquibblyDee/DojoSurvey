21cace53d0149f9cd0a221dc15cc47a7aeaf62c6
unknown <steelst0@gmail.com>
2018-08-02 12:27:28 -0700

    fixed my routing issue

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -5,8 +5,15 @@ namespace DojoSurvey.Controllers
     public class MainController : Controller
     {
         [HttpGet]
-        [Route("")]
+        [Route("/")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/result")]
+        public IActionResult Result(string Name, string Location, string FavLang, string Comment="")
         {
             return View();
         }
